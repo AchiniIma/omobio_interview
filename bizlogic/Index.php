c69a33f85929572a64133b5baf031c52ae439729
AchiniIma <achiniimalka@gmail.com>
2021-01-15 19:35:09 +0530

    init commit

--- bizlogic/Index.php
+++ bizlogic/Index.php
@@ -1,1 +1,14 @@
 <?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "exam";
+
+//Establishing connection
+$db_conn  = mysqli_connect($host, $user, $password, $dbname);
+
+//Connection not established
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
